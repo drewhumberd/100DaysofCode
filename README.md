a1d04c417a6aed144520dfdffc5626863590cd9a
Drew <45579108+drewhumberd@users.noreply.github.com>
2024-03-19 09:58:09 -0400

    day 14

--- README.md
+++ README.md
@@ -15,3 +15,4 @@ This repo is for projects created in Angela Yu's Udemy Python Bootcamp, availabl
   <li>Day 11 - Blackjack (Review of Learned Concepts So Far)</li>
   <li>Day 12 - Guess the Random Number (Scope)</li>
   <li>Day 13 - no direct project (Debugging)</li>
+  <li>Day 14 - Higher/Lower Game (Review of Learned Concepts So Far)</li>
---
+++ day14 - higherlower.py
@@ -0,0 +1,60 @@
+from game_data import data
+from os import system
+import random
+import art
+
+def get_contestant():
+    contestant = random.choice(data)
+    data.remove(contestant)
+    return contestant
+
+def contestant_string(dict):
+    return(f"{dict['name']}, a {dict['description']} from {dict['country']}")
+
+def player_win():
+    global playerscore
+    playerscore += 1
+    cont = input(f"You got it! Your score is {playerscore}. Keep playing? Y or N: ")
+    if cont == "n":
+        global is_playing
+        is_playing = False
+    else:
+        global replay
+        replay = True
+        system("cls")
+
+def player_lose():
+    print(f"Wrong! Game over. Your score was {playerscore}.")
+    global is_playing
+    is_playing = False
+
+playerscore = 0
+is_playing = True
+replay = False
+
+def maingame():
+    print(art.hl_logo)
+    if replay == True:
+        contestant_a = nextround
+    else:
+        contestant_a = get_contestant()
+    print(f"Compare A: {contestant_string(contestant_a)}")
+    print(art.hl_vs)
+    contestant_b = get_contestant()
+    print(f"To B: {contestant_string(contestant_b)}")
+    guess = input("Which do you think has more Instagram followers? Choose A or B: ")
+    if guess == "a":
+        if contestant_a['follower_count'] > contestant_b['follower_count']:
+            player_win()
+            return contestant_a
+        else:
+            player_lose()
+    else:
+        if contestant_b['follower_count'] > contestant_a['follower_count']:
+            player_win()
+            return contestant_b
+        else:
+            player_lose()
+
+while is_playing == True:
+    nextround = maingame()
